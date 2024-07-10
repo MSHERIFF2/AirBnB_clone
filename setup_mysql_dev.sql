e9d91dc945f500f1cebfb1d045e43b28d19be61e
Sheriffdeen Muritala Adegoke <muritalasheriffdeenadegoke@gmail.com>
2024-07-10 17:15:42 +0100

    Create setup_mysql_dev.sql

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,14 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
+-- Create the user if it doesn't exist and set the password
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
+-- Grant all privileges on the database to the user
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
+-- Grant SELECT privilege on performance_schema to the user
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
+-- Apply changes
+FLUSH PRIVILEGES;
